9be934da84d7dd91639bf12aec71cf06abad93b8
smartha864-a11y <smart.ha864@yahoo.com>
2026-04-16 13:17:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import psycopg2
 
 app = Flask(__name__)
 
-DATABASE_URL = "postgresql://xxxxxx"
+DATABASE_URL = DATABASE_URL = "postgresql://postgres:password@aws-xxx.pooler.supabase.com:6543/postgres"
 
 def get_conn():
     return psycopg2.connect(DATABASE_URL)
